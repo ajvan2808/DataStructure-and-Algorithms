1ac25117fa864bc8f9eda23106954f0e71c0b9b1
Hanh Van <vhh2808@gmail.com>
2023-04-26 14:03:50 +0700

    practice array

--- src/main/java/dynamicArrays.java
+++ src/main/java/dynamicArrays.java
@@ -1,12 +1,11 @@
 package src.main.java;
-/**
- * dynamicArrays
- */
-public class dynamicArrays<String> {
+
+public class dynamicArrays <String>{
     private Object[] data;
     private int size;
     private int initialCapacity;
 
+    // Constructor
     public dynamicArrays(int initialCapacity) {
         this.initialCapacity = initialCapacity;
         data = new Object[initialCapacity];
@@ -17,76 +16,77 @@ public class dynamicArrays<String> {
     }
 
     public void set(int index, String value) {
-        if (index < initialCapacity) {
-            data[index] = value;
-        } else {
-            // Error handling
-            throw new IndexOutOfBoundsException("Index is too big");
-        }
+        data[index] = value;
     }
 
-    // Insert to array
     public void insert(int index, String value) {
-        // check size
+        // Check size
         if (size == initialCapacity) {
             resize();
         }
-        // copying
-        for(int j = size; j > index; j--) {
-            data[j] = data[j-1];
+
+        // Copy/shift elements up by walk through the array from tail backward and move elements up
+        for (int i = size; i > index; i--) {
+            data[i] = data[i-1];
         }
-        // insert
+
+        // insert new value at index and increase array size by 1
         data[index] = value;
         size++;
+
     }
 
-    // Delete array element
+    public int size() {
+        return size;
+    }
+
+    // Dynamically resize the array 
+    public void resize() {
+        Object[] newData = new Object[initialCapacity * 2];
+        // copy elements from old arr to new one
+        for (int j = 0; j < initialCapacity; j++) {
+            newData[j] = data[j];
+        }
+
+        // re-assign the attributes
+        data = newData; 
+        initialCapacity = initialCapacity * 2;
+    }
+
+
+    // Delete arr elements
     public void delete(int index) {
-        for(int j = index; j < size-1; j++) {
-            data[j] = data[j+1];
+        // start copy down from the element index we need to delete
+        for (int i = index; i < size -1; i++) {
+            data[i] = data[i+1];
         }
         size--;
     }
 
-    // Check if array is empty
-    public boolean isEmpty(){
-        return size == 0;
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
     }
 
-    // Check if array contains the value
-    public boolean Contains(String value) {
-        for (int i = 0; i <= size -1; i++) {
-            String currentValue = (String) data[i];
-            if (currentValue.equals(value)) {
+    public boolean contains(String value) {
+        for (int i = 0; i < size; i++) {
+            String currentValue = (String)data[i];
+            if(currentValue.equals(value)) {
                 return true;
             }
         }
         return false;
     }
 
-    // Resize the array
-    private void resize() {
-        Object[] newData = new Object[initialCapacity * 2];
-        for(int i = 0; i < initialCapacity; i++) {
-            newData[i] = data[i];
-        }
-        data = newData;
-        initialCapacity = initialCapacity * 2;
-    }
-
-    // Get size of array
-    public int size() {
-        return size;
-    }
-
-    // Print array
-    public void print() {
+    // Additional actions
+    public void display() {
         for(int i = 0; i < size; i++) {
             System.out.println("data[i] = " + data[i]);
         }
     }
 
-    // Some array implementations offer method like add (also known as pushBack). Literally, insert at the end of the array
     public void add(String value) {
         if (size == initialCapacity) {
             resize();
